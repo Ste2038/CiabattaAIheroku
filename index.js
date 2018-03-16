a7da4bee2e72d9d41a14ba97dc2b515a8bbe7485
Ste2038 <giulianelliste@gmail.com>
2018-03-16 22:35:23 +0100

    Aggiunta risposta POST

--- index.js
+++ index.js
@@ -2,3 +2,19 @@ var app = require('express')();
 var basicAuth = require('express-basic-auth');
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
+
+app.use(basicAuth({
+  users: { 'admin': 'secret'}
+}));
+
+app.get('/', function(req, res){
+    res.send("ciao");
+});
+
+app.post('/', function(req, res){
+    console.log('POST / ', JSON.stringify(req.body));
+});
+
+http.listen(process.env.PORT || 3000, function(){
+    console.log('listening');
+});
